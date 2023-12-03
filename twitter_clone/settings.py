bc4fb8c944912822a50b7b87d8271d737b3b60e2
Ankit Suthar <sutharankit781@gmail.com>
2023-12-03 18:05:00 +0530

    half login page

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'Twitter_app',
 ]
 
 MIDDLEWARE = [
@@ -117,14 +119,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR,'static')
+    BASE_DIR / "static",
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '551743730468-33b1gr7qjovm05lvgvgogmu271qdrqra.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
